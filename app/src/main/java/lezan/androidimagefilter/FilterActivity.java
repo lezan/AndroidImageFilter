2509fe4a0f526c488a50a99cb26e755f7c36c7d8
lezan <zanchileonardo@gmail.com>
2016-10-29 22:19:07 +0200

    Init

--- app/src/main/java/lezan/androidimagefilter/FilterActivity.java
+++ app/src/main/java/lezan/androidimagefilter/FilterActivity.java
@@ -2,8 +2,6 @@ package lezan.androidimagefilter;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
@@ -20,14 +18,8 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.Toast;
-/*
-import org.opencv.android.OpenCVLoader;
-import org.opencv.android.Utils;
-import org.opencv.core.Mat;
-import org.opencv.imgproc.Imgproc;*/
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import org.opencv.android.OpenCVLoader;
 
 public class FilterActivity extends AppCompatActivity {
 
@@ -35,8 +27,6 @@ public class FilterActivity extends AppCompatActivity {
         System.loadLibrary("native-lib");
         System.loadLibrary("opencv_java3");
     }
-
-    private boolean sourceAlreadyRead = false;
 
     private int filterType = 1;
 
@@ -66,8 +56,12 @@ public class FilterActivity extends AppCompatActivity {
     private Button buttonFilter;
 
     private Uri uriImageCropped;
-    private InputStream stream = null;
-    //private Mat source = new Mat();
+
+    private String sourceImagePath;
+    private String outputImagePath;
+
+    private static int ADAPTIVE_THRESH_MEAN_C = 0;
+    private static int ADAPTIVE_THRESH_GAUSSIAN_C = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -117,95 +111,66 @@ public class FilterActivity extends AppCompatActivity {
         // Apply the adapter to the spinner
         snipperAdaptiveMethod.setAdapter(adapter);
 
-        setImageView();
+        setUriImageCropped();
+
+        setImageSourcePath();
 
         handleFilter();
     }
 
-    public void doneFilter(View view) {
-        handleFilter();
-    }
-
-    private void handleFilter() {
-
-        try {
-            stream = getContentResolver().openInputStream(uriImageCropped);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
-        if(!sourceAlreadyRead) {
-            handleBitmap(stream);
-            sourceAlreadyRead = true;
-        }
-
-        switch(filterType) {
-            case 0:
-                adaptiveThresholdMethod();
-                break;
-            case 1:
-                //xDOGMethod();
-                break;
-        }
-    }
-
-    private void setImageView() {
+    private void setUriImageCropped() {
 
         Bundle extras = getIntent().getExtras();
         if (extras != null && extras.containsKey("uriImageCropped")) {
             uriImageCropped = Uri.parse(extras.getString("uriImageCropped"));
         }
 
-        /*try {
+        /*
+        try {
             imageView1.setImageURI(uriImageCropped);
         } catch(Exception e) {
             e.printStackTrace();
-        }*/
+        }
+        */
 
     }
 
-    private void handleBitmap(InputStream stream) {
+    private void setImageSourcePath() {
+        sourceImagePath = getPath(uriImageCropped);
+    }
 
-        /* Creo bitmap a partire dall'URI */
-        BitmapFactory.Options bmpImageInputFactoryOptions = new BitmapFactory.Options();
-        bmpImageInputFactoryOptions.inPreferredConfig = Bitmap.Config.ARGB_8888;
-        Bitmap originalBitmapImage = BitmapFactory.decodeStream(stream, null, bmpImageInputFactoryOptions);
+    private void handleFilter() {
 
-        /* Converto l'immagine bitmap in una matrice */
-        //Utils.bitmapToMat(originalBitmapImage, source);
+        if (!OpenCVLoader.initDebug()) {
+            Log.d("OpenCV", "\n OpenCVLoader.initDebug(), not working.");
+        } else {
+            Log.d("OpenCV", "\n OpenCVLoader.initDebug(), WORKING.");
+
+            switch(filterType) {
+                case 0:
+                    adaptiveThresholdMethod();
+                    break;
+                case 1:
+                    xDOGMethod();
+                    break;
+            }
+
+            setImageViewOutput();
+        }
     }
 
     private void adaptiveThresholdMethod() {
-        /* Creo una nuova matrice a partire dalla matrice sorgente */
-        //Mat gray = new Mat();
-        /* Converto la matrice sorgente in scala di grigi */
-        //Imgproc.cvtColor(source, gray, Imgproc.COLOR_BGR2GRAY);
-
-        /* Applico una sfocatura all'immagine in scale di grigi */
-        //Imgproc.medianBlur(gray, gray, 5);
-        //Imgproc.GaussianBlur(gray, gray, new Size(5,5), 0);
-
-        /* Creo la matrice che conterrà il risultato finale */
-        //Mat result = new Mat();
-        /* Applico il threshold alla matrice in scala di grigi ottenendo un'immagine binaria */
-        //Imgproc.adaptiveThreshold(gray, result, 255, checkAdaptiveMethod(), Imgproc.THRESH_BINARY, getCheckBlockSize(), getCheckConstant());
-
-        /* Creo l'immagine bitmap a partire dalla matrice dell'immagine binaria */
-        //Bitmap filterImage = Bitmap.createBitmap(result.cols(), result.rows(), Bitmap.Config.ARGB_8888);
-        /* Converto la matrice a bitmap */
-        //Utils.matToBitmap(result, filterImage);
-        /* Setto il contenitore dell'immagine finale con il risultato ottenuto */
-        //imageView1.setImageBitmap(filterImage);
+        outputImagePath = adaptiveThreshold(sourceImagePath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/Artuino_photo", checkAdaptiveMethod(), getCheckBlockSize(), getCheckConstant());
     }
-/*
+
     private int checkAdaptiveMethod() {
         String method = snipperAdaptiveMethod.getSelectedItem().toString();
         if(method.equals("Mean")) {
-            return Imgproc.ADAPTIVE_THRESH_MEAN_C;
+            return ADAPTIVE_THRESH_MEAN_C;
         }
-        return Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C;
+        return ADAPTIVE_THRESH_GAUSSIAN_C;
     }
-*/
+
     private int getCheckBlockSize() {
         int blockSize = Integer.parseInt(editTextBlockSize.getText().toString());
         if((blockSize % 2) == 1 && blockSize > 0) {
@@ -224,7 +189,7 @@ public class FilterActivity extends AppCompatActivity {
     private double getCheckConstant() {
         return Double.parseDouble(editTextConstant.getText().toString());
     }
-/*
+
     private void xDOGMethod() {
 
         double kappa = Double.parseDouble(editTextKappa.getText().toString());
@@ -232,22 +197,9 @@ public class FilterActivity extends AppCompatActivity {
         double tau = Double.parseDouble(editTextTau.getText().toString());
         double phi = Double.parseDouble(editTextPhi.getText().toString());
 
-        //String imageSourcePath = getPath(uriImageCropped);
-
-        if (!OpenCVLoader.initDebug()) {
-            Log.d("OpenCV", "\n OpenCVLoader.initDebug(), not working.");
-        } else {
-            Log.d("OpenCV", "\n OpenCVLoader.initDebug(), WORKING.");
-
-            String xdogOutputImagePath = xdog(imageSourcePath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/Artuino_photo", kappa, sigma, tau, phi);
-
-            if(!xdogOutputImagePath.equals("False")) {
-                imageView1.setImageDrawable(null);
-                imageView1.setImageURI(Uri.parse(xdogOutputImagePath));
-            }
-        }
+        outputImagePath = xdog(sourceImagePath, Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/Artuino_photo", kappa, sigma, tau, phi);
     }
-*/
+
     private String getPath(Uri uri) {
         if(uri == null) {
             return null;
@@ -263,10 +215,21 @@ public class FilterActivity extends AppCompatActivity {
         return uri.getPath();
     }
 
-    public void goBack(View view) {
+    private void setImageViewOutput() {
+        if(!outputImagePath.equals("False")) {
+            imageView1.setImageDrawable(null);
+            imageView1.setImageURI(Uri.parse(outputImagePath));
+        }
+    }
 
+    public void doneFilter(View view) {
+        handleFilter();
+    }
+
+    public void goBack(View view) {
         finish();
     }
+
     public void sendArduino() {
 
     }
@@ -276,5 +239,6 @@ public class FilterActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
 
-    //public native String xdog(String imageSourcePath, String path, double kappa, double sigma, double tau, double phi);
+    public native String xdog(String imageSourcePath, String path, double kappa, double sigma, double tau, double phi);
+    public native String adaptiveThreshold(String imageSourcePath, String path, int adaptiveMethod, int blockSize, double constant);
 }
